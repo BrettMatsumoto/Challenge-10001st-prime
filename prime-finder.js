8e890ea33bf9944a5700886615941021818703d4
Brett <brett_matsumoto@yahoo.com>
2019-04-22 15:04:37 -1000

    made pull request changes

--- prime-finder.js
+++ prime-finder.js
@@ -2,17 +2,21 @@
 
 module.exports = function(n) {
   let prime = [2];
+  
   for (let i = 3; prime.length < n; i += 2) {
     let isPrime = true;
+    
     for (let j = 0; j < prime.length && prime[j] * prime[j] <= i; j++) {
       if (i % prime[j] === 0) {
         isPrime = false;
         break;
       }
     }
+
     if (isPrime) {
       prime.push(i);
     }
   }
+
   return prime.pop();
 };
